875f97a8f4e99e2b8e8940bc9c288d0f534d8856
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2025-07-14 00:42:10 -0400

    Implemented feedback loop for LLM

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,7 +2,10 @@ import os
 from google.genai import types
 
 def get_files_info(working_directory, directory = None):
-    full_path = os.path.join(working_directory,directory)
+    if directory:
+        full_path = os.path.join(working_directory,directory)
+    else:
+        full_path = working_directory
     parent_path = os.path.abspath(working_directory)
     child_path = os.path.abspath(full_path)
     if os.path.commonpath([parent_path]) != os.path.commonpath([parent_path, child_path]):
@@ -12,7 +15,7 @@ def get_files_info(working_directory, directory = None):
     contents = []
     try:
         for file in os.listdir(child_path):
-            is_directory = "True" if os.path.isdir(os.path.join(child_path,file)) else False
+            is_directory = "True" if os.path.isdir(os.path.join(child_path,file)) else "False"
             contents.append(f"- {file}: file_size={os.path.getsize(os.path.join(child_path,file))} bytes, is_dir={is_directory}")
     except Exception as e:
         return "Error: " + str(e) 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.call_functions import call_function,available_functions
+from config import MAX_ITERS
 
 
 def main():
@@ -32,23 +33,34 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """ 
-    
-    res = client.models.generate_content(model="gemini-2.0-flash-001",contents= messages,\
-                                         config=types.GenerateContentConfig(tools= [available_functions], system_instruction=system_prompt)) 
-    if verbose:
-        print("User prompt:",prompt)
-        print("Prompt tokens:",res.usage_metadata.prompt_token_count)
-        print("Response tokens:",res.usage_metadata.candidates_token_count)
-    if res.function_calls:
-        for fc in res.function_calls:
-            #print(f"Calling function: {fc.name}({fc.args})")
-            function_call_result = call_function(fc,verbose)
-            try:
-                response = function_call_result.parts[0].function_response.response
-                if verbose:
-                    print(f"-> {response}")
-            except Exception as e:
-                raise Exception(f"Error: calling function {fc.name}: {e}")
-    print(res.text)
+    i = 0
+    while True:
+        if i == MAX_ITERS:
+            print("Maximum number of iterations reached")
+            break
+        i+=1
+        res = client.models.generate_content(model="gemini-2.0-flash-001",contents= messages,\
+                                             config=types.GenerateContentConfig(tools= [available_functions], system_instruction=system_prompt)) 
+        if res.candidates:
+            for candidate in res.candidates:
+                messages.append(candidate.content)
+        if verbose:
+            print("User prompt:",prompt)
+            print("Prompt tokens:",res.usage_metadata.prompt_token_count)
+            print("Response tokens:",res.usage_metadata.candidates_token_count)
+        if res.function_calls:
+            for fc in res.function_calls:
+                #print(f"Calling function: {fc.name}({fc.args})")
+                function_call_result = call_function(fc,verbose)
+                try:
+                    response = function_call_result.parts[0].function_response.response
+                    if verbose:
+                        print(f"-> {response}")
+                    messages.append(function_call_result)
+                except Exception as e:
+                    raise Exception(f"Error: calling function {fc.name}: {e}")
+        else:
+            print(res.text)
+            break
 if __name__ == "__main__":
     main()
